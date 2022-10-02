4d6e6a0c578bd1ae4d582c79f490955599458cda
Jeff Malone <jeff@nov20.ly.lan>
2022-10-02 16:29:19 +0200

    remove dead code

--- clipboard-listener.py
+++ clipboard-listener.py
@@ -42,17 +42,6 @@ class ClipboardListenerTest(unittest.TestCase):
             actual = transform(_input)
 
             self.assertEqual(expected, actual, message)
-        # assertAlmostEqual assertAlmostEquals assertDictContainsSubset assertDictEqual
-        # assertEqual assertEquals assertFalse assertGreater assertGreaterEqual
-        # assertIn assertIs assertIsInstance assertIsNone assertIsNot assertIsNotNone
-        # assertItemsEqual assertLess assertLessEqual assertListEqual assertMultiLineEqual
-        # assertNotAlmostEqual assertNotAlmostEquals assertNotEqual assertNotEquals
-        # assertNotIn assertNotIsInstance assertNotRegexpMatches assertRaises
-        # assertRaisesRegexp assertRegexpMatches assertSequenceEqual assertSetEqual
-        # assertTrue assertTupleEqual assert_ countTestCases debug defaultTestResult
-        # doCleanups fail failIf failIfAlmostEqual failIfEqual failUnless failUnlessAlmostEqual
-        # failUnlessEqual failUnlessRaises failureException id longMessage maxDiff
-        # run setUp setUpClass shortDescription skipTest tearDown tearDownClass
 
 def logging_conf(
         level='INFO', # DEBUG
